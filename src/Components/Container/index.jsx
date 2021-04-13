ffdac970b1f1b423ab4ac81e4c0f9261d382ce23
Gabriel Pereira Bastos <gabrielpb88@gmail.com>
2021-04-12 23:03:13 -0300

    refatorado icones

--- src/Components/Container/index.jsx
+++ src/Components/Container/index.jsx
@@ -1,17 +1,26 @@
-import React from "react";
+import React from "react"
+import styled from "styled-components"
 
 import Titulo from "../Titulo";
 import Conta from "../Conta";
 
-const Container = () => {
-  return (
-    <div className="container">
-      <Titulo>Olá Fulano!</Titulo>
-      <section className="conteudo">
-        <Conta />
-      </section>
-    </div>
-  );
-};
+const Container = styled.div`
+  background-color: #f1f1f1;
+  min-height: 90vh;
+  padding: 0px 15vw;
+`
 
-export default Container;
+const Conteudo = styled.section`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`
+
+export default () => (
+  <Container>
+    <Titulo>Olá Fulano!</Titulo>
+    <Conteudo>
+      <Conta />
+    </Conteudo>
+  </Container>
+)
