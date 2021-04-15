5e0c8a7aa98f0d5865b9c051b5d62e8f8e320eb6
Gabriel Pereira Bastos <gabrielpb88@gmail.com>
2021-04-15 11:45:54 -0300

    adicionado alteracao de temas

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { ThemeProvider } from "styled-components";
 import {temaClaro, temaEscuro } from "./Components/UI/temas";
 
 import { GlobalStyle } from "./Components/GlobalStyle";
 import Cabecalho from "./Components/Cabecalho";
 import Container from "./Components/Container";
+import { BtnTema } from "./Components/UI";
+import SwitcherTema from './Components/SwitchTema'
 
 function App() {
+  const [tema, setTema] = useState(true)
+
+  const toggleTema = () => {
+    setTema((tema) => setTema(!tema))
+  }
+
   return (
     <>
-      <ThemeProvider theme={ temaClaro }>
+      <ThemeProvider theme={ tema ? temaClaro : temaEscuro }>
         <GlobalStyle/>
+        <BtnTema onClick={toggleTema}>
+          <SwitcherTema/>
+        </BtnTema>
         <Cabecalho />
         <Container />
       </ThemeProvider>
