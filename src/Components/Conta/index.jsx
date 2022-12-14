34616bce10d10c8eb8e3fad577011ce1f414efd2
Gabriel Pereira Bastos <gabrielpb88@gmail.com>
2022-12-14 17:24:44 -0300

    refact: update Conta component to use styled-component

--- src/Components/Conta/index.jsx
+++ src/Components/Conta/index.jsx
@@ -1,8 +1,19 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
+import styled from 'styled-components';
 
-import privado from "../../assets/images/privado.svg";
-import olho_icone from "../../assets/images/olho.svg";
-import dinheiro from "../../assets/images/dinheiro.svg";
+import privado from '../../assets/images/privado.svg';
+import olho_icone from '../../assets/images/olho.svg';
+import dinheiro from '../../assets/images/dinheiro.svg';
+import { Botao, Box, Detalhe, Icone, Saldo } from '../UI';
+
+const IconeMargin = styled(Icone)`
+  margin-top: 2px;
+`;
+
+const ContaWrapper = styled.div`
+  font-size: 26px;
+  padding: 20px 0;
+`;
 
 const Conta = () => {
   const [toggleState, untoggle] = useState(true);
@@ -12,29 +23,27 @@ const Conta = () => {
   };
 
   return (
-    <div className="box">
+    <Box>
       <h2>Conta</h2>
-      <div style={{ fontSize: "26px", padding: "20px 0" }}>
-        Saldo disponível{" "}
+      <ContaWrapper>
+        Saldo disponível{' '}
         <span>
-          <img className="imagem-icone" src={dinheiro} alt="Ícone Saldo" />
+          <Icone src={dinheiro} alt="Ícone Saldo" />
         </span>
-        {toggleState ? (
-          <div className="saldo">
-            <span className="detalhe">R$</span> 0,00{" "}
-          </div>
-        ) : null}
-      </div>
+        {toggleState && (
+          <Saldo>
+            <Detalhe>R$</Detalhe> 0,00{' '}
+          </Saldo>
+        )}
+      </ContaWrapper>
 
-      <button className="btn" onClick={toggleHandler}>
-        <img
-          style={{ marginTop: "2px" }}
-          className="imagem-icone"
+      <Botao onClick={toggleHandler}>
+        <IconeMargin
           src={toggleState ? privado : olho_icone}
           alt="Privacidade do Saldo"
         />
-      </button>
-    </div>
+      </Botao>
+    </Box>
   );
 };
 
