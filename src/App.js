e37fd43d9b174370c33f60ea1ccf44bb73a198d2
Gabriel Pereira Bastos <gabrielpb88@gmail.com>
2022-12-14 16:36:29 -0300

    feat: reset css using GlobalStyles from styled-component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from "react";
 
 import Cabecalho from "./Components/Cabecalho";
 import Container from "./Components/Container";
-
+import { GlobalStyles } from "./Components/GlobalStyles";
 function App() {
   return (
     <>
+      <GlobalStyles />
       <Cabecalho />
       <Container />
     </>
   );
 }
-
 export default App;
--- src/Components/Titulo/index.jsx
+++ src/Components/Titulo/index.jsx
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 
-export const Titulo = styled.h1`
-  color: grey; 
+const Titulo = styled.h1`
+  color: grey;
   padding: 25px 0;
-`
+`;
+
+export default Titulo;
